b0f482e81ff9c5c4732dba5deea1df222ef1f036
Ola Ajala <ola.ajala1@outlook.com>
2018-08-04 07:36:42 +0100

    Added ScrollToElement class

---
+++ src/test/java/GetAllAvailableLinks.java
@@ -0,0 +1,75 @@
+package test.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+
+public class GetAllAvailableLinks {
+
+    public static WebDriver driver;
+    public static Properties prop;
+
+    public GetAllAvailableLinks(){
+        try{
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    @BeforeMethod
+    public void setUp(){
+        String browserName = prop.getProperty("browser");
+
+        if (browserName.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/browsers/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")){
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"browsers/geckodriver");
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("safari")){
+            driver = new SafariDriver();
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(prop.getProperty("urlTravelex"));
+    }
+
+    @Test
+    public static void findAllLinksTest(){
+      List<WebElement> links =  driver.findElements(By.tagName("a"));
+      System.out.println("Total number of available links = " + links.size());
+
+      for(int i = 0; i < links.size(); i++){
+          String eachLink = links.get(i).getText();
+          System.out.println("link " + i + " = " + eachLink);
+      }
+    }
+
+    @AfterMethod (enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/ScrollToElement.java
@@ -0,0 +1,79 @@
+package test.java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+
+public class ScrollToElement {
+
+    public static WebDriver driver;
+    public static Properties prop;
+
+    public ScrollToElement() {
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + "/config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @BeforeMethod
+    public void setUp() {
+        String browserName = prop.getProperty("browser");
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/browsers/chromedriver");
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "browsers/geckodriver");
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("safari")) {
+            driver = new SafariDriver();
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(prop.getProperty("urlTravelex"));
+    }
+
+    @Test
+    public static void findAllLinksTest() {
+
+        WebElement contact = driver.findElement(By.linkText("Contact"));
+
+        scrollIntoView(contact, driver);
+        contact.click();
+    }
+
+    public static void scrollIntoView(WebElement element, WebDriver driver) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
